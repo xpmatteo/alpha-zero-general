008653aebcd91f32fb0a1758ef0c742a3303f82f
Matteo Vaccari <mvaccari@thoughtworks.com>
2023-08-21 12:31:49 +0200

    RunForTheTop: make it easier for a human to play against the computer

--- runforthetop/RunForTheTopPlayers.py
+++ runforthetop/RunForTheTopPlayers.py
@@ -16,38 +16,28 @@ class RandomPlayer():
             a = np.random.randint(self.game.getActionSize())
         return a
 
-
-def clean_input_move(input_move):
-    return input_move \
-        .replace("(", " ") \
-        .replace(")", " ") \
-        .replace(",", " ")
-
-
 class HumanPlayer():
     def __init__(self, game):
         self.game = game
 
     def play(self, board):
-        valid = self.game.getValidMoves(board, 1)
-        for i in range(len(valid)):
-            if valid[i]:
-                print("[", Game._from_numpy_action_to_move(i), end="] ")
+        actions = self.game.getValidMoves(board, 1)
+        valid_actions = []
+        for i in range(len(actions)):
+            if actions[i]:
+                valid_actions.append(i)
+        for i in range(len(valid_actions)):
+            print("%2d: %s" % (i, Game._from_numpy_action_to_move(valid_actions[i])))
         while True:
-            input_move = input()
-            input_a = clean_input_move(input_move).split()
-            if len(input_a) == 4:
-                try:
-                    x1,y1,x2,y2 = [int(i) for i in input_a]
-                    move = ((x1,y1), (x2, y2))
-                    action = Game._from_move_to_numpy_action( move )
-                    if valid[action]:
-                        break
-                except ValueError:
-                    # Input needs to be an integer
-                    'Invalid integer'
+            try:
+                input_move = valid_actions[int(input())]
+                if actions[input_move]:
+                    break
+            except ValueError:
+                # Input needs to be an integer
+                'Invalid integer'
             print('Invalid move')
-        return action
+        return input_move
 
 
 class GreedyPlayer():
