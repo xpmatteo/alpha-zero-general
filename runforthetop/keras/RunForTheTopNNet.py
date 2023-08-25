fe2b0b1afe79b55166c3727e12ce5395778d9f22
Matteo Vaccari <mvaccari@thoughtworks.com>
2023-08-25 08:16:13 +0200

    runforthetop: simplifying the NNet construction

--- runforthetop/keras/RunForTheTopNNet.py
+++ runforthetop/keras/RunForTheTopNNet.py
@@ -8,14 +8,13 @@ from tensorflow.keras.optimizers import *
 class RunForTheTopNNet():
     def __init__(self, game, args):
         # game params
-        self.board_x, self.board_y = game.getBoardSize()
-        self.action_size = game.getActionSize()
-        self.args = args
+        board_x, board_y = game.getBoardSize()
+        action_size = game.getActionSize()
 
         # Neural Net
-        self.input_boards = Input(shape=(self.board_x, self.board_y))    # s: batch_size x board_x x board_y
+        self.input_boards = Input(shape=(board_x, board_y))    # s: batch_size x board_x x board_y
 
-        x_image = Reshape((self.board_x, self.board_y, 1))(self.input_boards)                # batch_size  x board_x x board_y x 1
+        x_image = Reshape((board_x, board_y, 1))(self.input_boards)                # batch_size  x board_x x board_y x 1
         h_conv1 = Activation('relu')(BatchNormalization(axis=3)(Conv2D(args.num_channels, 3, padding='same', use_bias=False)(x_image)))         # batch_size  x board_x x board_y x num_channels
         h_conv2 = Activation('relu')(BatchNormalization(axis=3)(Conv2D(args.num_channels, 3, padding='same', use_bias=False)(h_conv1)))         # batch_size  x board_x x board_y x num_channels
         h_conv3 = Activation('relu')(BatchNormalization(axis=3)(Conv2D(args.num_channels, 3, padding='valid', use_bias=False)(h_conv2)))        # batch_size  x (board_x-2) x (board_y-2) x num_channels
@@ -23,7 +22,7 @@ class RunForTheTopNNet():
         h_conv4_flat = Flatten()(h_conv4)       
         s_fc1 = Dropout(args.dropout)(Activation('relu')(BatchNormalization(axis=1)(Dense(1024, use_bias=False)(h_conv4_flat))))  # batch_size x 1024
         s_fc2 = Dropout(args.dropout)(Activation('relu')(BatchNormalization(axis=1)(Dense(512, use_bias=False)(s_fc1))))          # batch_size x 1024
-        self.pi = Dense(self.action_size, activation='softmax', name='pi')(s_fc2)   # batch_size x self.action_size
+        self.pi = Dense(action_size, activation='softmax', name='pi')(s_fc2)   # batch_size x action_size
         self.v = Dense(1, activation='tanh', name='v')(s_fc2)                    # batch_size x 1
 
         self.model = Model(inputs=self.input_boards, outputs=[self.pi, self.v])
