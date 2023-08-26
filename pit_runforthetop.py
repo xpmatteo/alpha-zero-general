4dea92b6325ebedc997cb0989270e6e20b5653cc
Matteo Vaccari <mvaccari@thoughtworks.com>
2023-08-26 08:30:37 +0200

    RunForTheTop: make the number of mcts iterations in pit_runforthetop adjustable

--- pit_runforthetop.py
+++ pit_runforthetop.py
@@ -17,6 +17,7 @@ any agent.
 parser = argparse.ArgumentParser()
 parser.add_argument("--num_plays", type=int, default=2, help="number of games to run for")
 parser.add_argument("--human", action="store_true", default=False, help="human vs cpu")
+parser.add_argument("--mcts_iterations", type=int, default=50, help="number of MCTS iterations")
 args = parser.parse_args()
 
 human_vs_cpu = args.human
@@ -31,7 +32,7 @@ hp = HumanPlayer(g).play
 # nnet players
 n1 = NNet(g)
 n1.load_checkpoint('./temp', 'best.keras')
-args1 = dotdict({'numMCTSSims': 50, 'cpuct': 1.0})
+args1 = dotdict({'numMCTSSims': args.mcts_iterations, 'cpuct': 1.0})
 mcts1 = MCTS(g, n1, args1)
 n1p = lambda x: np.argmax(mcts1.getActionProb(x, temp=0))
 
